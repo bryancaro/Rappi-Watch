57affe1f1398ca7bf898d24c8b7043cc715eeccb
Bryan Caro <bryancaro@icloud.com>
2021-10-28 11:38:59 +0200

    Initial Commit

---
+++ Rappi Watch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rappi Watch
+//
+//  Created by Bryan Caro on 10/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rappi WatchUITests/Rappi_WatchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Rappi_WatchUITestsLaunchTests.swift
+//  Rappi WatchUITests
+//
+//  Created by Bryan Caro on 10/28/21.
+//
+
+import XCTest
+
+class Rappi_WatchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
